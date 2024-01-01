fe87dbe29d4779297600ee87d0a4e3770c34a855
william <will@spinspire.com>
2024-01-01 17:41:22 -0500

    project init

---
+++ frontend/src/routes/Greeting.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { Greet } from "$lib/wailsjs/go/main/App";
+
+  let name: string;
+  async function greet() {
+    alert(await Greet(name));
+  }
+</script>
+
+<form>
+  <input type="text" bind:value={name} />
+  <button on:click={greet}>Greet</button>
+</form>
